a0ff8b41f250ecec2396743bbe0e20e7e525394d
wuhan_xz <jinyijie902@sina.com>
2023-11-07 23:42:09 +0800

    feat: 新增

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    200,
+			"message": "test",
+			"data":    "xiaomajie",
+		})
+	})
+	router.Run(":8000")
+}
